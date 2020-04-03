57c28ae29c4ad14ffbf5565216181e4986518c77
Shubham Goel <shubham.goel@91springboard.com>
2020-04-04 00:14:16 +0530

    [add] code cleanup

--- src/main/java/com/quizcorona/ws/restapis/notification/V1NotificationWs.java
+++ src/main/java/com/quizcorona/ws/restapis/notification/V1NotificationWs.java
@@ -1,6 +1,8 @@
 package com.quizcorona.ws.restapis.notification;
+
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+
 import com.sun.jersey.api.representation.Form;
 import com.twilio.Twilio;
 import com.twilio.rest.api.v2010.account.Message;
@@ -12,6 +14,60 @@ public class V1NotificationWs {
     public static final String ACCOUNT_SID = "";
     public static final String AUTH_TOKEN = "";
 
+    public static String[] questionsArr = {"Welcome to the Corona quiz. \n Here is your first question:\n What is the apt distance that should be maintained between individuals?\n " +
+            "a1. 3 feet\n" +
+            "a2. 1 feet\n" +
+            "a3. 10 feet\n \n\n Reply with the correct option(for ex: 'a1' for 3 feet)", "Covid will always present itself as respiratory distress/difficulty in breathing?\n" +
+            "b1. Yes\nb2. No"};
+    private static String whatsappBotNumber = "whatsapp:+14155238886";
+
+    private static String introduction1 = "Hello, QuizCorona is a fun new way to learn about the novel Covid-19 virus. \n" +
+            "\n" +
+            "I will ask you questions that test your knowledge on Corona Virus and as you play, I will reward points that are converted into real money. \n" +
+            "\n" +
+            "The money you earn is then donated directly to the PMCares fund used to help those affected by the virus.\n" +
+            "So you get to help the nation while you learn something new. Win Win! \n";
+
+    private static String introduction2 = "For every right answer, you get 1 point and for every wrong, 1/4 points are deducted. If you don’t answer a question in 10 seconds, I move to the next question! To help you understand, I will also explain why each answer was right or wrong.\n" +
+            "\n" +
+            "Let’s play? Type Start to Start Quiz.\n";
+
+    public static String[] rightAnswerArray = {"",
+
+            "Good job, that’s the right answer, the apt distance that should be maintained between individuals is 3 feet." +
+                    " Here’s why : Explanation- When someone coughs or sneezes they spray small liquid droplets from their nose or mouth" +
+                    " which may contain virus. These droplets are too heavy to hang in the air. They quickly fall on floors or surfaces. The projectile of those droplets are 3 feet at the max.\nhttps://www.youtube.com/watch?time_continue=89&v=1APwq1df6Mw&feature=emb_logo",
+
+            "Right Answer",
+
+            "Right Answer",
+
+            "Right Answer",
+
+            "Right Answer"};
+
+    public static String[] wrongAnswerArray = {"",
+
+            "Oh oh, that doesn’t seem to be correct, the apt distance that should be maintained " +
+                    "between individuals is 3 feet. Here’s why\nExplanation- When someone coughs or sneezes they spray small liquid droplets" +
+                    " from their nose or mouth which may contain virus. These droplets are too heavy to hang in the air. " +
+                    "They quickly fall on floors or surfaces. The projectile of those droplets are 3 feet at the max." +
+                    "\nhttps://www.youtube.com/watch?time_continue=89&v=1APwq1df6Mw&feature=emb_logo",
+
+            "Wrong Answer",
+
+            "Wrong Answer. \nCorrect answer is 'No'\nExplanation-Till date, there is no evidence that a dog, cat or any pet can transmit COVID-19." +
+                    " COVID-19 is mainly spread through droplets produced when an infected person coughs, sneezes, or speaks.",
+
+            "Wrong Answer",
+
+            "Wrong Answer",
+
+            "Wrong Answer"};
+
+    private static String scoreMessage = "Feel free to share with your friends..\n" +
+            "Your total score is 1 point(s).\n" +
+            "Let’s move to the next question… \n";
 
     @Path("/notifyWhatsappBot")
     @POST
@@ -22,36 +78,21 @@ public class V1NotificationWs {
         Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
 
         if (formParam.get("Body").get(0).equalsIgnoreCase("hi")) {
-            Message message = Message.creator(
-                    new com.twilio.type.PhoneNumber(formParam.get("From").get(0)),
-                    new com.twilio.type.PhoneNumber("whatsapp:+14155238886"),
-                    "Welcome to the Corona quiz. \n Here is your first question:\n What is the apt distance that should be maintained between individuals?\n " +
-                            "a1. 3 feet\n" +
-                            "a2. 1 feet\n" +
-                            "a3. 10 feet\n \n\n Reply with the correct option(for ex: 'a1' for 3 feet)")
-                    .create();
-            System.out.println(message.getSid());
-
+            sendMessage(formParam.get("From").get(0), whatsappBotNumber, introduction1);
+            sendMessage(formParam.get("From").get(0), whatsappBotNumber, introduction2);
+        } else if (formParam.get("Body").get(0).equalsIgnoreCase("start")) {
+            sendMessage(formParam.get("From").get(0), whatsappBotNumber, questionsArr[0]);
         }
         // First question response
         else if (formParam.get("Body").get(0).equalsIgnoreCase("a1")
                 || formParam.get("Body").get(0).equalsIgnoreCase("a2")
                 || formParam.get("Body").get(0).equalsIgnoreCase("a3")) {
             if (formParam.get("Body").get(0).equalsIgnoreCase("a1")) {
-                Message message = Message.creator(
-                        new com.twilio.type.PhoneNumber(formParam.get("From").get(0)),
-                        new com.twilio.type.PhoneNumber("whatsapp:+14155238886"),
-                        "Right Answer")
-                        .create();
-                System.out.println(message.getSid());
+                sendMessage(formParam.get("From").get(0), whatsappBotNumber, rightAnswerArray[1]);
             } else {
-                Message message = Message.creator(
-                        new com.twilio.type.PhoneNumber(formParam.get("From").get(0)),
-                        new com.twilio.type.PhoneNumber("whatsapp:+14155238886"),
-                        "Wrong Answer. \nCorrect answer is 3 Feet\nExplanation- When someone coughs or sneezes they spray small liquid droplets from their nose or mouth which may contain virus. These droplets are too heavy to hang in the air. They quickly fall on floors or surfaces. The projectile of those droplets are 3 feet at the max.\nhttps://www.youtube.com/watch?time_continue=89&v=1APwq1df6Mw&feature=emb_logo")
-                        .create();
-                System.out.println(message.getSid());
+                sendMessage(formParam.get("From").get(0), whatsappBotNumber, wrongAnswerArray[1]);
             }
+            sendMessage(formParam.get("From").get(0), whatsappBotNumber, scoreMessage);
 
             sendNextQuestion(2, formParam.get("From").get(0));
 
@@ -60,20 +101,13 @@ public class V1NotificationWs {
         else if (formParam.get("Body").get(0).equalsIgnoreCase("b1")
                 || formParam.get("Body").get(0).equalsIgnoreCase("b2")) {
             if (formParam.get("Body").get(0).equalsIgnoreCase("b2")) {
-                Message message = Message.creator(
-                        new com.twilio.type.PhoneNumber(formParam.get("From").get(0)),
-                        new com.twilio.type.PhoneNumber("whatsapp:+14155238886"),
-                        "Right Answer")
-                        .create();
-                System.out.println(message.getSid());
+                sendMessage(formParam.get("From").get(0), whatsappBotNumber, rightAnswerArray[2]);
             } else {
-                Message message = Message.creator(
-                        new com.twilio.type.PhoneNumber(formParam.get("From").get(0)),
-                        new com.twilio.type.PhoneNumber("whatsapp:+14155238886"),
-                        "Wrong Answer. \nCorrect answer is 'No'\nExplanation-Only 1 out of 6 people who gets Covid will develop respiratory distress.\n")
-                        .create();
-                System.out.println(message.getSid());
+                sendMessage(formParam.get("From").get(0), whatsappBotNumber, wrongAnswerArray[2]);
+
             }
+            sendMessage(formParam.get("From").get(0), whatsappBotNumber, scoreMessage);
+
             sendNextQuestion(3, formParam.get("From").get(0));
 
         }
@@ -81,61 +115,39 @@ public class V1NotificationWs {
         else if (formParam.get("Body").get(0).equalsIgnoreCase("c1")
                 || formParam.get("Body").get(0).equalsIgnoreCase("c2")) {
             if (formParam.get("Body").get(0).equalsIgnoreCase("c2")) {
-                Message message = Message.creator(
-                        new com.twilio.type.PhoneNumber(formParam.get("From").get(0)),
-                        new com.twilio.type.PhoneNumber("whatsapp:+14155238886"),
-                        "Right Answer")
-                        .create();
-                System.out.println(message.getSid());
+                sendMessage(formParam.get("From").get(0), whatsappBotNumber, rightAnswerArray[3]);
+
             } else {
-                Message message = Message.creator(
-                        new com.twilio.type.PhoneNumber(formParam.get("From").get(0)),
-                        new com.twilio.type.PhoneNumber("whatsapp:+14155238886"),
-                        "Wrong Answer. \nCorrect answer is 'No'\nExplanation-Till date, there is no evidence that a dog, cat or any pet can transmit COVID-19. COVID-19 is mainly spread through droplets produced when an infected person coughs, sneezes, or speaks.")
-                        .create();
-                System.out.println(message.getSid());
+                sendMessage(formParam.get("From").get(0), whatsappBotNumber, wrongAnswerArray[3]);
             }
+            sendMessage(formParam.get("From").get(0), whatsappBotNumber, scoreMessage);
+
             sendNextQuestion(4, formParam.get("From").get(0));
         }
         // Fourth question response
         else if (formParam.get("Body").get(0).equalsIgnoreCase("d1")
                 || formParam.get("Body").get(0).equalsIgnoreCase("d2")) {
             if (formParam.get("Body").get(0).equalsIgnoreCase("d2")) {
-                Message message = Message.creator(
-                        new com.twilio.type.PhoneNumber(formParam.get("From").get(0)),
-                        new com.twilio.type.PhoneNumber("whatsapp:+14155238886"),
-                        "Right Answer")
-                        .create();
-                System.out.println(message.getSid());
+                sendMessage(formParam.get("From").get(0), whatsappBotNumber, rightAnswerArray[4]);
+
             } else {
-                Message message = Message.creator(
-                        new com.twilio.type.PhoneNumber(formParam.get("From").get(0)),
-                        new com.twilio.type.PhoneNumber("whatsapp:+14155238886"),
-                        "Wrong Answer. \nCorrect answer is 'No'\nExplanation-Coronaviruses are a group of related viruses that cause diseases in mammals and birds. In humans, coronaviruses cause respiratory tract infections that can be mild, such as some cases of the common cold (among other possible causes, predominantly rhinoviruses), and others that can be lethal, such as SARS, MERS, and COVID-19.\n" +
-                                "It’s called novel because Covid-19 is a new kind of coronavirus that has been found to infect humans.")
-                        .create();
-                System.out.println(message.getSid());
+                sendMessage(formParam.get("From").get(0), whatsappBotNumber, wrongAnswerArray[4]);
+
             }
+            sendMessage(formParam.get("From").get(0), whatsappBotNumber, scoreMessage);
+
             sendNextQuestion(5, formParam.get("From").get(0));
         }
         // Fifth question response
         else if (formParam.get("Body").get(0).equalsIgnoreCase("e1")
                 || formParam.get("Body").get(0).equalsIgnoreCase("e2")) {
             if (formParam.get("Body").get(0).equalsIgnoreCase("e2")) {
-                Message message = Message.creator(
-                        new com.twilio.type.PhoneNumber(formParam.get("From").get(0)),
-                        new com.twilio.type.PhoneNumber("whatsapp:+14155238886"),
-                        "Right Answer")
-                        .create();
-                System.out.println(message.getSid());
+                sendMessage(formParam.get("From").get(0), whatsappBotNumber, rightAnswerArray[5]);
             } else {
-                Message message = Message.creator(
-                        new com.twilio.type.PhoneNumber(formParam.get("From").get(0)),
-                        new com.twilio.type.PhoneNumber("whatsapp:+14155238886"),
-                        "Wrong Answer. \nCorrect answer is 'No'\nExplanation- People can have Covid-19 infection and may not show any symptoms. They’re called healthy carriers. While they may not show Covid related symptoms at all, but they can transmit the virus to others.")
-                        .create();
-                System.out.println(message.getSid());
+                sendMessage(formParam.get("From").get(0), whatsappBotNumber, wrongAnswerArray[5]);
             }
+            sendMessage(formParam.get("From").get(0), whatsappBotNumber, scoreMessage);
+
             //TODO: save response in db
             sendQuizComplete(formParam.get("From").get(0));
         }
@@ -144,75 +156,21 @@ public class V1NotificationWs {
     }
 
     void sendNextQuestion(int nextQuestion, String toPhone) {
-        switch (nextQuestion) {
-            case 2: {
-                new java.util.Timer().schedule(
-                        new java.util.TimerTask() {
-                            @Override
-                            public void run() {
-                                Message messageNextQuestion2 = Message.creator(
-                                        new com.twilio.type.PhoneNumber(toPhone),
-                                        new com.twilio.type.PhoneNumber("whatsapp:+14155238886"),
-                                        "Covid will always present itself as respiratory distress/difficulty in breathing?\n" +
-                                                "b1. Yes\nb2. No")
-                                        .create();
-                            }
-                        },
-                        1000
-                );
-            }
-            break;
-            case 3: {
-                new java.util.Timer().schedule(
-                        new java.util.TimerTask() {
-                            @Override
-                            public void run() {
-                                Message messageNextQuestion2 = Message.creator(
-                                        new com.twilio.type.PhoneNumber(toPhone),
-                                        new com.twilio.type.PhoneNumber("whatsapp:+14155238886"),
-                                        "3)   Covid can spread through pets like cats and dogs?    \n" +
-                                                "c1. Yes\nc2. No")
-                                        .create();
-                            }
-                        },
-                        1000
-                );
-            }
-            break;
-            case 4: {
-                new java.util.Timer().schedule(
-                        new java.util.TimerTask() {
-                            @Override
-                            public void run() {
-                                Message messageNextQuestion2 = Message.creator(
-                                        new com.twilio.type.PhoneNumber(toPhone),
-                                        new com.twilio.type.PhoneNumber("whatsapp:+14155238886"),
-                                        "Coronavirus is new and has spread to humans for the first time?\n" +
-                                                "d1. Yes\nd2. No")
-                                        .create();
-                            }
-                        },
-                        1000
-                );
-            }
-            break;
-            case 5: {
-                new java.util.Timer().schedule(
-                        new java.util.TimerTask() {
-                            @Override
-                            public void run() {
-                                Message messageNextQuestion2 = Message.creator(
-                                        new com.twilio.type.PhoneNumber(toPhone),
-                                        new com.twilio.type.PhoneNumber("whatsapp:+14155238886"),
-                                        "If a person has no flu like symptoms, she/he cannot transmit covid-19 to others?\n" +
-                                                "e1. Yes\ne2. No")
-                                        .create();
-                            }
-                        },
-                        1000
-                );
-            }
-        }
+
+        new java.util.Timer().schedule(
+                new java.util.TimerTask() {
+                    @Override
+                    public void run() {
+                        Message messageNextQuestion2 = Message.creator(
+                                new com.twilio.type.PhoneNumber(toPhone),
+                                new com.twilio.type.PhoneNumber(whatsappBotNumber),
+                                questionsArr[nextQuestion - 1])
+                                .create();
+                    }
+                },
+                1000
+        );
+
     }
 
     void sendQuizComplete(String toPhone) {
@@ -230,5 +188,18 @@ public class V1NotificationWs {
                 1000
         );
     }
+
+    void sendMessage(String to, String from, String message) {
+        try {
+            Message messageCreator = Message.creator(
+                    new com.twilio.type.PhoneNumber(to),
+                    new com.twilio.type.PhoneNumber(from),
+                    message).create();
+            System.out.println(messageCreator.getSid());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
 
 }
