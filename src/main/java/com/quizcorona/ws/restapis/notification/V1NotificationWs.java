e28119a2880c2c7e4cb6a2e2f41b880387a27573
Shubham Goel <shubham.goel@91springboard.com>
2020-04-04 16:10:33 +0530

    [fix] added database support to store sessions

--- src/main/java/com/quizcorona/ws/restapis/notification/V1NotificationWs.java
+++ src/main/java/com/quizcorona/ws/restapis/notification/V1NotificationWs.java
@@ -3,9 +3,14 @@ package com.quizcorona.ws.restapis.notification;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 
+import com.datastax.driver.core.*;
+import com.datastax.driver.core.querybuilder.QueryBuilder;
+import com.quizcorona.ws.persister.cassandra.DbService;
 import com.sun.jersey.api.representation.Form;
 import com.twilio.Twilio;
 import com.twilio.rest.api.v2010.account.Message;
+
+import java.util.Date;
 
 @Path("/v1")
 
@@ -14,11 +19,23 @@ public class V1NotificationWs {
     public static final String ACCOUNT_SID = "";
     public static final String AUTH_TOKEN = "";
 
-    public static String[] questionsArr = {"Welcome to the Corona quiz. \n Here is your first question:\n What is the apt distance that should be maintained between individuals?\n " +
+    public static String[] questionsArr = {"Here is your first question:\n What is the apt distance that should be maintained between individuals?\n " +
             "a1. 3 feet\n" +
             "a2. 1 feet\n" +
-            "a3. 10 feet\n \n\n Reply with the correct option(for ex: 'a1' for 3 feet)", "Covid will always present itself as respiratory distress/difficulty in breathing?\n" +
-            "b1. Yes\nb2. No"};
+            "a3. 10 feet\n \n\n Reply with the correct option(for ex: 'a1' for 3 feet)"
+
+            , "Covid will always present itself as respiratory distress/difficulty in breathing?\n" +
+            "b1. Yes\nb2. No",
+
+            "Covid can spread through pets like cats and dogs? \n" +
+                    "c1. Yes\nc2. No",
+
+            "Coronavirus is new and has spread to humans for the first time? \n" +
+                    "d1. Yes\nd2. No",
+
+            " If a person has no flu like symptoms, she/he cannot transmit covid-19 to others?\n" +
+                    "e1. Yes\ne2. No"
+    };
     private static String whatsappBotNumber = "whatsapp:+14155238886";
 
     private static String introduction1 = "Hello, QuizCorona is a fun new way to learn about the novel Covid-19 virus. \n" +
@@ -32,42 +49,48 @@ public class V1NotificationWs {
             "\n" +
             "Let’s play? Type Start to Start Quiz.\n";
 
-    public static String[] rightAnswerArray = {"",
-
-            "Good job, that’s the right answer, the apt distance that should be maintained between individuals is 3 feet." +
-                    " Here’s why : Explanation- When someone coughs or sneezes they spray small liquid droplets from their nose or mouth" +
-                    " which may contain virus. These droplets are too heavy to hang in the air. They quickly fall on floors or surfaces. The projectile of those droplets are 3 feet at the max.\nhttps://www.youtube.com/watch?time_continue=89&v=1APwq1df6Mw&feature=emb_logo",
-
-            "Right Answer",
-
-            "Right Answer",
-
-            "Right Answer",
-
-            "Right Answer"};
-
     public static String[] wrongAnswerArray = {"",
 
             "Oh oh, that doesn’t seem to be correct, the apt distance that should be maintained " +
-                    "between individuals is 3 feet. Here’s why\nExplanation- When someone coughs or sneezes they spray small liquid droplets" +
-                    " from their nose or mouth which may contain virus. These droplets are too heavy to hang in the air. " +
-                    "They quickly fall on floors or surfaces. The projectile of those droplets are 3 feet at the max." +
+                    "between individuals is 3 feet." +
                     "\nhttps://www.youtube.com/watch?time_continue=89&v=1APwq1df6Mw&feature=emb_logo",
 
-            "Wrong Answer",
+            "No, Covid will not always present itself as respiratory distress/difficulty in breathing\n",
 
-            "Wrong Answer. \nCorrect answer is 'No'\nExplanation-Till date, there is no evidence that a dog, cat or any pet can transmit COVID-19." +
-                    " COVID-19 is mainly spread through droplets produced when an infected person coughs, sneezes, or speaks.",
+            "No, Covid cannot spread through pets like cats and dogs",
 
-            "Wrong Answer",
+            "No, Coronavirus is a known group of viruses that has been infecting humans for ages.",
 
-            "Wrong Answer",
+            "No, a person with no flu like symptoms can also transmit covid-19 to others",
 
             "Wrong Answer"};
 
-    private static String scoreMessage = "Feel free to share with your friends..\n" +
-            "Your total score is 1 point(s).\n" +
-            "Let’s move to the next question… \n";
+    public static String[] rightAnswerArray = {"",
+
+            "Good job, that’s the right answer, the apt distance that should be maintained between individuals is 3 feet.\n"
+                    + "\nhttps://www.youtube.com/watch?time_continue=89&v=1APwq1df6Mw&feature=emb_logo",
+
+            "Good Job, Covid will not always present itself as respiratory distress/difficulty in breathing\n"
+            ,
+
+            "Good Job, Covid cannot spread through pets like cats and dogs",
+
+            "Good Job, Coronavirus is a known group of viruses that has been infecting humans for ages.",
+
+            "Good Job, a person with no flu like symptoms can also transmit covid-19 to others",
+
+            "Right Answer"};
+
+    private static String scoreMessage = "Your total score is *";
+
+    private static String scoreEnd = " point(s)*.";
+
+    public static String KEYSPACENAME = "sb91_kysp";
+    private String tableName = "quizcorona";
+    private int plusPoints = 4;
+    private int minusPoints = -1;
+
+
 
     @Path("/notifyWhatsappBot")
     @POST
@@ -75,24 +98,32 @@ public class V1NotificationWs {
     public void notifyWhatsappBot(Form formParam) {
         System.out.println(formParam);
         // create request to send message
-        Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
 
         if (formParam.get("Body").get(0).equalsIgnoreCase("hi")) {
             sendMessage(formParam.get("From").get(0), whatsappBotNumber, introduction1);
             sendMessage(formParam.get("From").get(0), whatsappBotNumber, introduction2);
         } else if (formParam.get("Body").get(0).equalsIgnoreCase("start")) {
-            sendMessage(formParam.get("From").get(0), whatsappBotNumber, questionsArr[0]);
+            if (!checkIfQuizAlreadyStarted(formParam.get("From").get(0))) {
+                sendMessage(formParam.get("From").get(0), whatsappBotNumber, questionsArr[0]);
+                System.out.print("create db entry for question 1 to a person");
+                createUserQuizStartEntry(formParam.get("From").get(0));
+            } else {
+                sendMessage(formParam.get("From").get(0), whatsappBotNumber, "You have already started the quiz. Your score is " + getUserScore(formParam.get("From").get(0)));
+            }
+
         }
         // First question response
         else if (formParam.get("Body").get(0).equalsIgnoreCase("a1")
                 || formParam.get("Body").get(0).equalsIgnoreCase("a2")
                 || formParam.get("Body").get(0).equalsIgnoreCase("a3")) {
             if (formParam.get("Body").get(0).equalsIgnoreCase("a1")) {
+                updateUserScore(formParam.get("From").get(0), plusPoints);
                 sendMessage(formParam.get("From").get(0), whatsappBotNumber, rightAnswerArray[1]);
             } else {
+                updateUserScore(formParam.get("From").get(0), minusPoints);
                 sendMessage(formParam.get("From").get(0), whatsappBotNumber, wrongAnswerArray[1]);
             }
-            sendMessage(formParam.get("From").get(0), whatsappBotNumber, scoreMessage);
+            sendMessage(formParam.get("From").get(0), whatsappBotNumber, scoreMessage + getUserScore(formParam.get("From").get(0)) + scoreEnd);
 
             sendNextQuestion(2, formParam.get("From").get(0));
 
@@ -101,12 +132,16 @@ public class V1NotificationWs {
         else if (formParam.get("Body").get(0).equalsIgnoreCase("b1")
                 || formParam.get("Body").get(0).equalsIgnoreCase("b2")) {
             if (formParam.get("Body").get(0).equalsIgnoreCase("b2")) {
+                updateUserScore(formParam.get("From").get(0), plusPoints);
+
                 sendMessage(formParam.get("From").get(0), whatsappBotNumber, rightAnswerArray[2]);
             } else {
+                updateUserScore(formParam.get("From").get(0), minusPoints);
+
                 sendMessage(formParam.get("From").get(0), whatsappBotNumber, wrongAnswerArray[2]);
 
             }
-            sendMessage(formParam.get("From").get(0), whatsappBotNumber, scoreMessage);
+            sendMessage(formParam.get("From").get(0), whatsappBotNumber, scoreMessage + getUserScore(formParam.get("From").get(0)) + scoreEnd);
 
             sendNextQuestion(3, formParam.get("From").get(0));
 
@@ -115,12 +150,16 @@ public class V1NotificationWs {
         else if (formParam.get("Body").get(0).equalsIgnoreCase("c1")
                 || formParam.get("Body").get(0).equalsIgnoreCase("c2")) {
             if (formParam.get("Body").get(0).equalsIgnoreCase("c2")) {
+                updateUserScore(formParam.get("From").get(0), plusPoints);
+
                 sendMessage(formParam.get("From").get(0), whatsappBotNumber, rightAnswerArray[3]);
 
             } else {
+                updateUserScore(formParam.get("From").get(0), minusPoints);
+
                 sendMessage(formParam.get("From").get(0), whatsappBotNumber, wrongAnswerArray[3]);
             }
-            sendMessage(formParam.get("From").get(0), whatsappBotNumber, scoreMessage);
+            sendMessage(formParam.get("From").get(0), whatsappBotNumber, scoreMessage + getUserScore(formParam.get("From").get(0)) + scoreEnd);
 
             sendNextQuestion(4, formParam.get("From").get(0));
         }
@@ -128,13 +167,17 @@ public class V1NotificationWs {
         else if (formParam.get("Body").get(0).equalsIgnoreCase("d1")
                 || formParam.get("Body").get(0).equalsIgnoreCase("d2")) {
             if (formParam.get("Body").get(0).equalsIgnoreCase("d2")) {
+                updateUserScore(formParam.get("From").get(0), plusPoints);
+
                 sendMessage(formParam.get("From").get(0), whatsappBotNumber, rightAnswerArray[4]);
 
             } else {
+                updateUserScore(formParam.get("From").get(0), minusPoints);
+
                 sendMessage(formParam.get("From").get(0), whatsappBotNumber, wrongAnswerArray[4]);
 
             }
-            sendMessage(formParam.get("From").get(0), whatsappBotNumber, scoreMessage);
+            sendMessage(formParam.get("From").get(0), whatsappBotNumber, scoreMessage + getUserScore(formParam.get("From").get(0)) + scoreEnd);
 
             sendNextQuestion(5, formParam.get("From").get(0));
         }
@@ -142,17 +185,54 @@ public class V1NotificationWs {
         else if (formParam.get("Body").get(0).equalsIgnoreCase("e1")
                 || formParam.get("Body").get(0).equalsIgnoreCase("e2")) {
             if (formParam.get("Body").get(0).equalsIgnoreCase("e2")) {
+                updateUserScore(formParam.get("From").get(0), plusPoints);
+
                 sendMessage(formParam.get("From").get(0), whatsappBotNumber, rightAnswerArray[5]);
             } else {
+                updateUserScore(formParam.get("From").get(0), minusPoints);
                 sendMessage(formParam.get("From").get(0), whatsappBotNumber, wrongAnswerArray[5]);
             }
-            sendMessage(formParam.get("From").get(0), whatsappBotNumber, scoreMessage);
+            sendMessage(formParam.get("From").get(0), whatsappBotNumber, scoreMessage + getUserScore(formParam.get("From").get(0)) + scoreEnd);
 
             //TODO: save response in db
             sendQuizComplete(formParam.get("From").get(0));
         }
 
         return;
+    }
+
+    private boolean checkIfQuizAlreadyStarted(String phone) {
+
+        Statement statement = QueryBuilder.select().all().from(KEYSPACENAME, "quizcorona")
+                .where(QueryBuilder.eq("id", phone));
+
+        ResultSet results = DbService.getInstance().getSession().execute(statement);
+        for (Row row : results) {
+            System.out.print("row-> " + row);
+            return true;
+        }
+        return false;
+    }
+
+    int getUserScore(String phone) {
+
+        Statement statement = QueryBuilder.select().all().from(KEYSPACENAME, tableName)
+                .where(QueryBuilder.eq("id", phone));
+
+        ResultSet results = DbService.getInstance().getSession().execute(statement);
+        for (Row row : results) {
+            return row.getInt("total_points");
+        }
+
+        return 0;
+    }
+
+    int updateUserScore(String phone, int pointsToAdd) {
+        Statement stmt = QueryBuilder.update(DbService.CLUSTER_NAME, tableName)
+                .with(QueryBuilder.set("total_points", getUserScore(phone) + pointsToAdd)).where(QueryBuilder.eq("id", phone));
+        DbService.getInstance().getSession().execute(stmt);
+        System.out.println(getUserScore(phone));
+        return getUserScore(phone);
     }
 
     void sendNextQuestion(int nextQuestion, String toPhone) {
@@ -181,7 +261,7 @@ public class V1NotificationWs {
                         Message messageNextQuestion2 = Message.creator(
                                 new com.twilio.type.PhoneNumber(toPhone),
                                 new com.twilio.type.PhoneNumber("whatsapp:+14155238886"),
-                                "Congratulations. You have completes the quiz. blah blah blah...")
+                                "Congratulations. You have completes the quiz. Your contribution to the fund is 50 Rupees")
                                 .create();
                     }
                 },
@@ -191,6 +271,8 @@ public class V1NotificationWs {
 
     void sendMessage(String to, String from, String message) {
         try {
+            Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
+
             Message messageCreator = Message.creator(
                     new com.twilio.type.PhoneNumber(to),
                     new com.twilio.type.PhoneNumber(from),
@@ -199,6 +281,22 @@ public class V1NotificationWs {
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    void createUserQuizStartEntry(String phone) {
+        PreparedStatement insertStatement = DbService.getInstance().getSession()
+                .prepare("INSERT INTO sb91_kysp.quizcorona"
+                        + "(id ,phone, last_question, is_quiz_completed, replyTime, total_points, questionsentTime)"
+                        + "VALUES (?, ?, ?, ?, ?, ?, ?);");
+        BoundStatement boundStatement = new BoundStatement(insertStatement);
+        DbService.getInstance().getSession()
+                .execute(boundStatement.bind(phone, phone, "1", false, new Date().getTime(), 0, new Date().getTime()));
+
+
+    }
+
+    void deleteAllSessions(){
+
     }
 
 
